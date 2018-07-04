e9e803be72f8d58e2a6d10f46c001ec6c55bf67c
Mohammad Rahimyarza Bagagarsyah <rahimyarz@gmail.com>
2018-07-04 16:23:30 +0700

    Added some variables and add some functions on viewDidLoad()

--- Register/ProfileViewController.swift
+++ Register/ProfileViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    var firstName: String = ""
+    var lastName: String = ""
+    var address: String = ""
+    var gender: Int = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    nameLabel.text = "\(firstName) \(lastName)"
+    addressLabel.text = address
+        if gender == 0 {
+            genderLabel.text = "Male"
+        }
+        else if gender == 1 {
+            genderLabel.text = "Female"
+        }
+        else if gender == 2 {
+            genderLabel.text = "Others"
+        }
         // Do any additional setup after loading the view.
     }
 
