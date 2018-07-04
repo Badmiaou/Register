443b338f8adcdd0b358a6953b6fa7cdb923af20b
Kennyzi Yusuf <ykennyzi@gmail.com>
2018-07-04 16:25:46 +0700

    edit variable on view controller

--- Register/ViewController.swift
+++ Register/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var genderOutlet: UISegmentedControl!
+    @IBOutlet weak var firstNameOutlet: UITextField!
+    @IBOutlet weak var lastNameOutlet: UITextField!
+    @IBOutlet weak var addressOutlet: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    var genderIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    // Mark -Register Button
+    
+    @IBAction func RegisterAction(_ sender: Any) {
+        if(firstNameOutlet.text == ""){
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
